9790be6455124460787a9b7892e4cc2fe3b9ecfe
Lukas Lowinger <llowinge@redhat.com>
2023-08-10 20:35:29 +0200

    CQ changes

--- openshift-ci/run.sh
+++ openshift-ci/run.sh
@@ -3,12 +3,13 @@ set -eux
 
 export _JAVA_OPTIONS=-Duser.home=$HOME
 
-mvn -B -V clean verify -fae \
+mvn -B -V clean install -fae -Dformat.skip=true -am -Dquarkus.openshift.build-log-level=DEBUG -ntp -Dts.global.log.nocolor=true \
     -Dmaven.repo.local=$PWD/local-repo \
     -Dquarkus.platform.group-id=$QUARKUS_PLATFORM_GROUP_ID \
     -Dquarkus.platform.artifact-id=$QUARKUS_PLATFORM_ARTIFACT_ID \
     -Dquarkus.platform.version=$QUARKUS_VERSION \
-    -Dquarkus-plugin.version=$QUARKUS_VERSION \
-    -Proot-modules,http-modules,sql-db-modules,monitoring-modules \
+    -Dcamel-quarkus.platform.group-id=$CAMEL_QUARKUS_PLATFORM_GROUP_ID \
+    -Dcamel-quarkus.platform.artifact-id=$CAMEL_QUARKUS_PLATFORM_ARTIFACT_ID \
+    -Dcamel-quarkus.platform.version=$CAMEL_QUARKUS_VERSION \
     -Dopenshift \
-    -pl $PROJECTS
+    -Pocp-interop
